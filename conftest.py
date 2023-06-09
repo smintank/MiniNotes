2ea6ca906ee75b3012fd9049ed6090fa7febb3bc
smintank <assbestmail@gmail.com>
2023-06-09 16:13:28 +0300

    Add config of pytest add pytest routes tests

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+from notes.models import Note
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def author_client(author, client):  # Вызываем фикстуру автора и клиента.
+    client.force_login(author)  # Логиним автора в клиенте.
+    return client
+
+
+@pytest.fixture
+def note(author):
+    note = Note.objects.create(
+        title='Заголовок',
+        text='Текст заметки',
+        slug='note-slug',
+        author=author,
+    )
+    return note
---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,46 @@
+from http import HTTPStatus
+
+import pytest
+from django.urls import reverse
+
+
+def test_home_availability_for_anonymous_user(client):
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.parametrize(
+    'name', ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(admin_client, name):
+    url = reverse(name)
+    response = admin_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('admin_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
