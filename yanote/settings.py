53673791b52b8a548e983f9f933a081ceb50f518
smintank <assbestmail@gmail.com>
2024-01-15 09:32:58 +0300

    Change name and main color of website

--- yanote/settings.py
+++ yanote/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-yipnj$#j!ajarq%k55z4kuf3x79)91h0h42o9!1ho(z=!%mt=#'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
